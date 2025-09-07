6bdf27e847eaf6b071e35f7b5d99bb512ec13491
pramila259 <dagarpramila326@gmail.com>
2025-09-07 17:17:37 +0530

    Update lookup.js

--- api/lookup.js
+++ api/lookup.js
@@ -1,7 +1,7 @@
 import { neon } from '@neondatabase/serverless';
 
 // Use your Neon database connection string
-const DATABASE_URL = process.env.DATABASE_URL || 'postgresql://neondb_owner:npg_ybgmB53IFcJx@ep-damp-tree-a2d12bz9.eu-central-1.aws.neon.tech/neondb?sslmode=require';
+const DATABASE_URL = process.env.DATABASE_URL || 'postgresql://neondb_owner:npg_0emZHsKUwy8V@ep-falling-brook-a1555d6b-pooler.ap-southeast-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require';
 
 const sql = neon(DATABASE_URL);
 
